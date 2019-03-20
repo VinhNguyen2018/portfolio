76bbf5a52a1f040baa5fa887231c48ac6aca3168
Francois Nguyen <francois.nguyen2@gmail.com>
2019-03-20 18:02:46 +0100

    added AOS on img cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,8 @@
 import "bootstrap";
 import { scrollMoveNav } from '../components/navbar_scroll';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
 scrollMoveNav();
+AOS.init();
 
