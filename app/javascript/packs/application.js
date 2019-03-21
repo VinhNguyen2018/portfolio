fe703f58d953ac58ef9e604563a958bcedc0dddd
Francois Nguyen <francois.nguyen2@gmail.com>
2019-03-21 11:10:09 +0100

    added smooth scroll js on link anchor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,10 @@
 import "bootstrap";
 import { scrollMoveNav } from '../components/navbar_scroll';
+import { smoothScroll } from '../components/smooth_scroll';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 scrollMoveNav();
+smoothScroll();
 AOS.init();
 
